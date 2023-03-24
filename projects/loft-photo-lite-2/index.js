6fcb1d5a0b016671837bc5f24d68e60c62357757
Юсуф Джанибеков <yusufdjanibekov@MacBook-Air-Usuf.local>
2023-03-24 14:00:28 +0300

    checked week-3

--- projects/loft-photo-lite-2/index.js
+++ projects/loft-photo-lite-2/index.js
@@ -2,6 +2,7 @@ import pages from './pages'
 import('./styles.css');
 
 const pageNames = ['login', 'main', 'profile'];
+
 document.addEventListener('click', () => {
   const pageName = model.getRandomElement(pageNames);
   pages.openPage(pageName);
